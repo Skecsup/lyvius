364c582176af5c0fb23e44a5ae630cbf6aee8e39
Gyula Kicsindy <kicsindy.julius@gmail.com>
2024-09-30 15:35:50 +0200

    setup shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-  return <div>lyvius</div>;
+  return (
+    <>
+      <div>lyvius</div>
+      <Button>Click me</Button>
+    </>
+  )
 }
