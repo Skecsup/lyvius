b741e70b34fc10752e6064173582b8d148e7b8ae
Gyula Kicsindy <kicsindy.julius@gmail.com>
2024-10-07 08:00:52 +0200

    create new user

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -3,8 +3,47 @@
 import { isRedirectError } from 'next/dist/client/components/redirect'
 
 import { signIn, signOut } from '@/auth'
-import { signInFormSchema } from '../validator'
+import { signInFormSchema, signUpFormSchema } from '../validator'
+import { hashSync } from 'bcrypt-ts-edge'
+import { formatError } from '../utils'
+import db from '@/db/drizzle'
+import { users } from '@/db/schema'
 
+// CREATE
+export async function signUp(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      confirmPassword: formData.get('confirmPassword'),
+      password: formData.get('password'),
+    })
+    const values = {
+      id: crypto.randomUUID(),
+      ...user,
+      password: hashSync(user.password, 10),
+    }
+    await db.insert(users).values(values)
+    await signIn('credentials', {
+      email: user.email,
+      password: user.password,
+    })
+    return { success: true, message: 'User created successfully' }
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error
+    }
+    return {
+      success: false,
+      message: formatError(error).includes(
+        'duplicate key value violates unique constraint "user_email_idx"'
+      )
+        ? 'Email is already exist'
+        : formatError(error),
+    }
+  }
+}
+//LOGIN
 export async function signInWithCredentials(
   prevState: unknown,
   formData: FormData
