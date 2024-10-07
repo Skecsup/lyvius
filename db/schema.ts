0ff902800c0736dc9e4aa7b612b1b44f92c047da
Gyula Kicsindy <kicsindy.julius@gmail.com>
2024-10-07 11:19:57 +0200

    implement cart functionality

--- db/schema.ts
+++ db/schema.ts
@@ -1,4 +1,5 @@
 //import { primaryKey } from 'drizzle-orm/pg-core/primary-keys'
+import { CartItem } from '@/types'
 import {
   boolean,
   integer,
@@ -9,6 +10,7 @@ import {
   uniqueIndex,
   uuid,
   primaryKey,
+  json,
 } from 'drizzle-orm/pg-core'
 
 import type { AdapterAccount } from 'next-auth/adapters'
@@ -95,3 +97,21 @@ export const products = pgTable(
     }
   }
 )
+
+// CARTS
+export const carts = pgTable('cart', {
+  id: uuid('id').notNull().defaultRandom().primaryKey(),
+  userId: uuid('userId').references(() => users.id, {
+    onDelete: 'cascade',
+  }),
+  sessionCartId: text('sessionCartId').notNull(),
+  items: json('items').$type<CartItem[]>().notNull().default([]),
+  itemsPrice: numeric('itemsPrice', { precision: 12, scale: 2 }).notNull(),
+  shippingPrice: numeric('shippingPrice', {
+    precision: 12,
+    scale: 2,
+  }).notNull(),
+  taxPrice: numeric('taxPrice', { precision: 12, scale: 2 }).notNull(),
+  totalPrice: numeric('totalPrice', { precision: 12, scale: 2 }).notNull(),
+  createdAt: timestamp('createdAt').notNull().defaultNow(),
+})
--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,13 @@
-import { products } from '@/db/schema'
+import { carts, products } from '@/db/schema'
+import { cartItemSchema } from '@/lib/validator'
 import { InferSelectModel } from 'drizzle-orm'
+import { z } from 'zod'
 
 // PRODUCTS
 export type Product = InferSelectModel<typeof products>
+
+//CART item
+export type CartItem = z.infer<typeof cartItemSchema>
+
+// CART
+export type Cart = InferSelectModel<typeof carts>
