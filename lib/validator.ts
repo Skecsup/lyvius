7d6a345c394c99b76d37ec68e573c18138ab6a92
Gyula Kicsindy <kicsindy.julius@gmail.com>
2024-10-08 09:07:03 +0200

    order details and place order page

--- lib/validator.ts
+++ lib/validator.ts
@@ -1,6 +1,8 @@
 import * as z from 'zod'
 import { formatNumberWithDecimal } from './utils'
 import { PAYMENT_METHODS } from './constants'
+import { createInsertSchema } from 'drizzle-zod'
+import { orderItems, orders } from '@/db/schema'
 
 // USER
 export const signInFormSchema = z.object({
@@ -57,3 +59,24 @@ export const paymentMethodSchema = z
     path: ['type'],
     message: 'Invalid payment method',
   })
+
+export const paymentResultSchema = z.object({
+  id: z.string(),
+  status: z.string(),
+  email_address: z.string(),
+  pricePaid: z.string(),
+})
+export const insertOrderSchema = createInsertSchema(orders, {
+  shippingAddress: shippingAddressSchema,
+  paymentResult: z
+    .object({
+      id: z.string(),
+      status: z.string(),
+      email_address: z.string(),
+      pricePaid: z.string(),
+    })
+    .optional(),
+})
+export const insertOrderItemSchema = createInsertSchema(orderItems, {
+  price: z.number(),
+})
